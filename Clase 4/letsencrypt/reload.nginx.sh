0e74c058fbcfa0ceb90955f98aa84d836937df11
Sebastian Arena <sarena@chat-tonic.com>
2020-12-02 10:01:20 -0300

    Clase 4

---
+++ Clase 4/letsencrypt/reload.nginx.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo docker container exec nginx nginx -s reload
---
+++ Clase 4/letsencrypt/view.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+sudo /usr/local/bin/docker run --rm --env-file $DIR/.env -v ~/config/letsencrypt/etc/letsencrypt:/etc/letsencrypt certbot/dns-route53 certificates
